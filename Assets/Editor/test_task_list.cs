c8883c6a21834d27d11aa302f0fd5044e72090e4
youichi <masudayouichi41@gmail.com>
2018-08-18 10:51:36 +0900

    コメント
    コメント追加

--- Assets/Editor/test_task_list.cs
+++ Assets/Editor/test_task_list.cs
@@ -9,15 +9,32 @@ using UnityEngine.TestTools;
 
 public class test_task_list {
 
-    [Test]
-    public void test_init () {
-        task_list test_obj;
-        test_obj = new task_list ();
-
+    /** 
+    @brief テスト用のファイル名を取得
+    @return string ファイル名
+    @details 特になし
+    */
+    private string get_test_file_name (bool isDisplayFileName = false) {
         utility my_util = new utility ();
         my_util.set_file_name (Application.persistentDataPath + "\\test\\");
 
-        test_obj.init (my_util.get_file_name ());
+        if (isDisplayFileName) {
+            Debug.Log ("テスト用ファイル");
+            Debug.Log (Application.persistentDataPath);
+        }
+        return my_util.get_file_name ();
+    }
+
+    [Test]
+    public void test_init () {
+        //初期化
+        task_list test_obj;
+        test_obj = new task_list ();
+
+        //処理
+        test_obj.init (get_test_file_name ());
+
+        //テスト
     }
 
     [Test]
@@ -27,18 +44,18 @@ public class test_task_list {
 
     [Test]
     public void test_load_task_list () {
+        //初期化
         task_list test_obj;
         test_obj = new task_list ();
 
-        utility my_util = new utility ();
-        my_util.set_file_name (Application.persistentDataPath + "\\test\\");
-
-        test_obj.init (my_util.get_file_name ());
+        test_obj.init (get_test_file_name ());
 
         int num = test_obj.get_item_count ();
 
+        //処理
         test_obj.load_task_list (num);
 
+        //テスト
         Assert.AreEqual ("1", test_obj.my_task_list[0].ID);
         Assert.AreEqual ("task_name1", test_obj.my_task_list[0].name);
         Assert.AreEqual ("my_detail1", test_obj.my_task_list[0].detail);
@@ -52,58 +69,57 @@ public class test_task_list {
 
     [Test]
     public void test_save_task_list () {
+        //初期化
         task_list test_obj;
         test_obj = new task_list ();
+        test_obj.init (get_test_file_name ());
 
-        utility my_util = new utility ();
-        my_util.set_file_name (Application.persistentDataPath + "\\test\\");
+        //処理
+        //test_obj.save_task_list ("D:\\Unity\\Android_Sample\\Assets\\resource\\output_text.txt"
 
-        test_obj.init (my_util.get_file_name ());
-
-        //test_obj.save_task_list ("D:\\Unity\\Android_Sample\\Assets\\resource\\output_text.txt");
+        //テスト
     }
 
     [Test]
     public void test_get_item_count () {
+        //初期化
         task_list test_obj;
         test_obj = new task_list ();
+        test_obj.init (get_test_file_name ());
 
-        utility my_util = new utility ();
-        my_util.set_file_name (Application.persistentDataPath + "\\test\\");
-
-        test_obj.init (my_util.get_file_name ());
-
+        //処理
         int num = test_obj.get_item_count ();
 
+        //テスト
         Assert.AreEqual (4, num);
     }
 
     [Test]
     public void test_delete_item () {
+        //初期化
         task_list test_obj;
         test_obj = new task_list ();
+        test_obj.init (get_test_file_name ());
 
-        utility my_util = new utility ();
-        my_util.set_file_name (Application.persistentDataPath + "\\test\\");
-
-        test_obj.init (my_util.get_file_name ());
-
+        //処理
         test_obj.delete_item ("1");
         test_obj.get_item_count ();
+
+        //テスト
         Assert.AreEqual (3, test_obj.get_item_count ());
     }
 
     [Test]
     public void test_add_item () {
+        //初期化
         task_list test_obj;
         test_obj = new task_list ();
+        test_obj.init (get_test_file_name ());
 
-        utility my_util = new utility ();
-        my_util.set_file_name (Application.persistentDataPath + "\\test\\");
-
-        test_obj.init (my_util.get_file_name ());
-
+        //処理
         test_obj.add_item ("5", "new_item", "new_detail", "new_status");
+
+        //テスト
         Assert.AreEqual (5, test_obj.get_item_count ());
     }
 }
